60708cd567ee018973af1d022bb619d9901dba2e
Travis Cook <cooktw@missouri.edu>
2023-03-29 12:08:00 -0400

    Fix case

--- inc/acf/group-definitions/acf-ds-layer-definitions.php
+++ inc/acf/group-definitions/acf-ds-layer-definitions.php
@@ -673,7 +673,7 @@ if ( function_exists( 'acf_add_local_field_group' ) ) :
 				array(
 					'key'               => 'field_6423009912246',
 					'label'             => 'More Events Link',
-					'name'              => 'Button',
+					'name'              => 'button',
 					'aria-label'        => '',
 					'type'              => 'link',
 					'instructions'      => '',
